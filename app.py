8a2015d105cb04f690034362eff59b9268c73f92
MuLIAICHI <mustaphaliaichi@gmail.com>
2024-01-09 17:20:15 +0100

    files

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def process_query(query):
     index.storage_context.persist()
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
     index0 = load_index_from_storage(storage_context=storage_context)
-    query_engine = index0.as_query_engine()
+    query_engine = index.as_query_engine()
     response = query_engine.query(query)
     return response
 
