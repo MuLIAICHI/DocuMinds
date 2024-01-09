670026493665be0d71307b114d6697e6187d5d15
MuLIAICHI <mustaphaliaichi@gmail.com>
2024-01-09 15:11:15 +0100

    adding the coudbinary config

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Cloudinary configuration
-cloudinary.config_from_url(os.getenv('CLOUDINARY_URL'))
+cloudinary.config(
+    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'), 
+    api_key = os.getenv('CLOUDINARY_API_KEY'), 
+    api_secret = os.getenv('CLOUDINARY_API_SECRET')
+)
 
 app = Flask(__name__)
 
